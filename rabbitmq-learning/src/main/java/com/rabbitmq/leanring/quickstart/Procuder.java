40572694b64a33d02c526ae96624ba4e61b92f7e
colorful9767 <1605333199@qq.com>
2020-02-15 17:24:30 +0800

    删除多余文件

--- rabbitmq-learning/src/main/java/com/rabbitmq/leanring/quickstart/Procuder.java
+++
@@ -1,7 +0,0 @@
-package com.rabbitmq.leanring.quickstart;
-
-/**
- * Created by perl on 2020-02-15.
- */
-public class Procuder {
-}
--- rabbitmq-starter/src/main/java/Test.java
+++
@@ -1,7 +0,0 @@
-package PACKAGE_NAME;
-
-/**
- * Created by perl on 2020-02-15.
- */
-public class Test {
-}
