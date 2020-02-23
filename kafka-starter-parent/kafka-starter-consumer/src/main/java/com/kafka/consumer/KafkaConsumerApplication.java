daefdd8d9a232b10e73a0edb756a23bebf1ce2b6
colorful9767 <1605333199@qq.com>
2020-02-23 13:29:22 +0800

    初始化kafka工程

---
+++ kafka-starter-parent/kafka-starter-consumer/src/main/java/com/kafka/consumer/KafkaConsumerApplication.java
@@ -0,0 +1,15 @@
+package com.kafka.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by perl on 2020-02-23.
+ */
+@SpringBootApplication
+public class KafkaConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaConsumerApplication.class, args);
+    }
+}
